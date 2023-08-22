029ddd01c774ea293e562bf7becf93a460b10238
Siddhesh Dongare <siddheshdongare43@gmail.com>
2023-08-22 17:12:00 +0530

    Created a File Explorer which create a view only Tree of the specified path.

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import javax.swing.*;
+import javax.swing.tree.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            // Create JFrame
+            JFrame frame = new JFrame();
+            frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            // Create JTree
+            DefaultMutableTreeNode root = new DefaultMutableTreeNode("Root");
+            JTree tree = new JTree(root);
+
+            // Add tree to JScrollPane
+            JScrollPane scrollPane = new JScrollPane(tree);
+
+            // Add JScrollPane to JFrame
+            frame.getContentPane().add(scrollPane);
+
+            // Set JFrame visible
+            frame.setVisible(true);
+
+            // Populate tree with files and folders
+            getFileTree(root, new File("C://Users//Siddhesh//Desktop"));
+        });
+    }
+
+    private static void getFileTree(DefaultMutableTreeNode root, File directory) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                DefaultMutableTreeNode subNode = new DefaultMutableTreeNode(file.getName());
+                root.add(subNode);
+                System.out.println(file.getName());
+                if (file.isDirectory()) {
+                    getFileTree(subNode, file);
+                }
+            }
+        }
+    }
+}
